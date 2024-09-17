9ec2d4d4f59c91621128c49a2095097f40999675
JosephEmer05 <ej.tarlengco@gmail.com>
2024-09-17 16:49:10 +0800

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is where you see our information</p>
+      </div>
+    );
+  };
+  
+  export default About;
+  
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+const Contact = () => {
+    return (
+      <div>
+        <h1>Contact Us</h1>
+        <form>
+          <label>Name: </label>
+          <input type="text" name="Enter your name" />
+          <label>Email: </label>
+          <input type="email" email="Enter your email" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Contact;
+  
